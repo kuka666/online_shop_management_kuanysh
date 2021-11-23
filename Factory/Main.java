9b237adbae9c4d93ad0e7ad7c5c2a6515d972eaf
alisher8 <80209649+alisher8@users.noreply.github.com>
2021-11-23 16:29:12 +0600

    Update Main.java

--- Factory/Main.java
+++ Factory/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         Factory factory = new Factory();
+        FactoryMethodJeans factoryjeans = new FactoryMethodJeans();
+
 
         TShirts whiteLshirt = factory.getType("WhiteL");
         System.out.println(whiteLshirt.ChooseTShirt());
@@ -22,5 +24,23 @@ public class Main {
 
         TShirts blackLshirt = factory.getType("BlackL");
         System.out.println(blackLshirt.ChooseTShirt());
+        
+        Jeans blueSjeans = factoryjeans.getType("BlueSJeans");
+        System.out.println(blueSjeans.ChooseJeans());
+
+        Jeans blueLjeans = factoryjeans.getType("BlueLJeans");
+        System.out.println(blueLjeans.ChooseJeans());
+
+        Jeans blueMjeans = factoryjeans.getType("BlueMJeans");
+        System.out.println(blueMjeans.ChooseJeans());
+
+        Jeans blackSjeans = factoryjeans.getType("BlackSJeans");
+        System.out.println(blackSjeans.ChooseJeans());
+
+        Jeans blackMjeans = factoryjeans.getType("BlackMJeans");
+        System.out.println(blackMjeans.ChooseJeans());
+
+        Jeans blackLjeans = factoryjeans.getType("BlackLJeans");
+        System.out.println(blackLjeans.ChooseJeans());
     }
 }
