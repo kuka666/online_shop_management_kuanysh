4022695fae86b9e6b0a7ffe1720edb30805d0863
Geroigazy <80098706+Geroigazy@users.noreply.github.com>
2021-11-25 15:59:55 +0600

    check Cart

--- Methods.java
+++ Methods.java
@@ -145,6 +145,75 @@ public class Methods {
             ex.printStackTrace();
         }
     }
+    
+        public void check_cart(int id) {
+        try(Connection connection = connect()) {
+            String sql = "select * from cart where customer_id = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, id);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()){
+                System.out.println(rs.getString("type")
+                        + " Price: " + rs.getFloat("price") + " Description: " + rs.getString("desc"));
+            }
+            System.out.println("Choose action(1-4):");
+            System.out.println("Make purchase");
+            System.out.println("Add product");
+            System.out.println("Remove product");
+            System.out.println("Exit");
+            int ch = scan.nextInt();
+            switch (ch){
+                case 1:
+                    purchase(id);
+                    break;
+                case 2:
+                    //add
+                    break;
+                case 3:
+                    //remove
+                    break;
+                default:
+                    System.exit(1);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void purchase(int id){
+        try(Connection connection = connect()) {
+            String sql = "select sum(price) as total from cart where customer_id = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1,id);
+            ResultSet rs = statement.executeQuery();
+            if(rs.next())
+                System.out.println("Total price: " + rs.getFloat("total"));
+            System.out.println("(1)buy\n(2)cancel:");
+            int ch = scan.nextInt();
+            switch (ch){
+                case 1:
+                    String sql1 = "delete from cart where customer_id = ?";
+                    statement = connection.prepareStatement(sql1);
+                    statement.setInt(1, id);
+                    int rs1 = statement.executeUpdate();
+                    String sql2 = "insert into cart (customer_id) values(?)";
+                    statement = connection.prepareStatement(sql2);
+                    statement.setInt(1, id);
+                    int rowsInserted = statement.executeUpdate();
+                    if (rowsInserted > 0) { // checking data additions to the database
+                        System.out.print("Successful order!!!");
+                    }
+                    break;
+                case 2:
+                    check_cart(id);
+                default:
+                    System.exit(1);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 
     public void checkNotify(int ids) {
         try (Connection connection = connect()) {
